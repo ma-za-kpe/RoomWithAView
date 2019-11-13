0ce8a3090bdc45c222524a2bcb626077951dad9d
MAZAKPE <makpalyy@gmail.com>
2019-11-13 10:44:59 +0200

    Add recyclerview adapter

---
+++ app/src/main/java/com/maku/roomwithaview/word/WordListAdapter.kt
@@ -0,0 +1,42 @@
+package com.maku.roomwithaview.word
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.maku.roomwithaview.R
+import com.maku.roomwithaview.database.Word
+
+class WordListAdapter internal constructor(
+    context: Context
+) : RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var words = emptyList<Word>() // Cached copy of words
+
+    inner class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val wordItemView: TextView = itemView.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): WordListAdapter.WordViewHolder {
+        val itemView = inflater.inflate(R.layout.recyclerview_item, parent, false)
+        return WordViewHolder(itemView)
+    }
+
+    override fun getItemCount() = words.size
+
+    override fun onBindViewHolder(holder: WordListAdapter.WordViewHolder, position: Int) {
+        val current = words[position]
+        holder.wordItemView.text = current.word
+    }
+
+    internal fun setWords(words: List<Word>) {
+        this.words = words
+        notifyDataSetChanged()
+    }
+}
